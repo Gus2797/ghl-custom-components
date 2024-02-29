bc96b27bae9141a2555676ab0a40a0e3289ef631
Gustavo Hernandez <ghdzplus@gmail.com>
2024-02-29 12:34:04 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Componentes creados en el curso de React Pro
+
+Instalar
+
+Npm
+```
+npm i ghl-custom-components
+```
+Yarn
+```
+yarn add ghl-custom-components
+```
+1. [Componente de botón](#boton)
+1. [Componente de input](#input)
+1. [Componente de select](#select)
---
+++ src/stories/MyLabel.stories.ts
@@ -0,0 +1,58 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { MyLabel, type Props } from '../components/MyLabel';
+
+const meta: Meta<Props> = {
+    title: 'UI/labels/MyLabel',
+    component: MyLabel,   
+    tags: ['autodocs'],
+    parameters: {
+        layout: 'centered',
+    },
+    argTypes: {
+        size: { control: 'inline-radio' },
+        fontColor: { control: 'color' },
+    }
+} satisfies Meta<typeof MyLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+    args: {
+        size: 'h1',
+        label: 'Basic label',
+    }
+};
+
+export const AllCaps: Story = {
+    args: {
+        size: 'h1',
+        label: 'All caps label',
+        allCaps: true,
+    }
+};
+
+export const Secondary: Story = {
+    args: {
+        size: 'h1',
+        label: 'Secondary label',
+        color: 'text-tertiary',
+    }
+};
+
+export const CustomColor: Story = {
+    args: {
+        size: 'h1',
+        label: 'Custom color label',
+        fontColor: 'red',
+    }
+};
+
+export const CustomBackgroundColor: Story = {
+    args: {
+        size: 'h1',
+        label: 'Custom background color',
+        fontColor: 'white',
+        backgroundColor: 'black',
+    }
+};
